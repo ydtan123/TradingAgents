1444c6aa70b85cf4443a5fb67f97f17e6b723512
Yudong Tan <yudong.tan@gmail.com>
2026-03-28 18:28:06 +0800

    feat: export create_analyst_team from agents package

--- tradingagents/agents/__init__.py
+++ tradingagents/agents/__init__.py
@@ -5,6 +5,7 @@ from .analysts.fundamentals_analyst import create_fundamentals_analyst
 from .analysts.market_analyst import create_market_analyst
 from .analysts.news_analyst import create_news_analyst
 from .analysts.social_media_analyst import create_social_media_analyst
+from .analysts.analyst_team import create_analyst_team
 
 from .researchers.bear_researcher import create_bear_researcher
 from .researchers.bull_researcher import create_bull_researcher
@@ -23,6 +24,7 @@ __all__ = [
     "create_msg_delete",
     "InvestDebateState",
     "RiskDebateState",
+    "create_analyst_team",
     "create_bear_researcher",
     "create_bull_researcher",
     "create_research_manager",
